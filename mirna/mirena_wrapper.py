ee511b5ee535d8d8044a1f1d797b026dcbc78aa1
David Minkley <dminkley@uvic.ca>
2018-08-17 13:40:10 -0700

    More argparse written.

--- mirna/mirena_wrapper.py
+++ mirna/mirena_wrapper.py
@@ -13,8 +13,16 @@ from Bio import SeqIO
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("genome_file", help="fasta file to search for miRNAs")
+    prog_description = (
+        "This script is a multiprocess whole-genome wrapper for MIReNA,"
+        " specifically for the functionality which searches for miRNAs within"
+        " a text string. Output pre-miRNA fasta sequence headers are modified"
+        " from the MIReNA default to include the genomic contig/scaffold ID"
+        " and position of the found pre-miRNA."
+        )
+    
+    parser = argparse.ArgumentParser(description=prog_description)
+    parser.add_argument("genome_file", metavar="METAVAR_TEST", help="fasta file to search for miRNAs")
     parser.add_argument("mirna_file",
                         help="fasta file containing miRNA sequences")
     parser.add_argument("--n_procs", "-p", help="number of processors",
