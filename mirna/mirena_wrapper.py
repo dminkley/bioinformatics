cb53c7c1e440b03421fdb065d50457b58bf6c017
David Minkley <dminkley@MacBook-2.local>
2018-08-20 16:45:35 -0700

    Final argparse from last week

--- mirna/mirena_wrapper.py
+++ mirna/mirena_wrapper.py
@@ -11,17 +11,16 @@ import argparse
 
 from Bio import SeqIO
 
+def parse_arguments():
+    """Parse sys.argv for arguments"""
 
-def main():
     prog_description = (
         "This script is a multiprocess whole-genome wrapper for MIReNA, specifically for the"
         " functionality which searches for miRNAs within a text string. Output pre-miRNA fasta"
         " sequence headers are modified from the MIReNA default to include the genomic"
         " contig/scaffold ID and position of the found pre-miRNA."
         )
-    
     parser = argparse.ArgumentParser(description=prog_description, add_help=False)
-    
     args_required = parser.add_argument_group("Required")
     args_required.add_argument("-g", "--genome", required=True, dest="genome_fn", metavar="FASTA",
                                help="fasta file to search for miRNAs")
@@ -35,11 +34,21 @@ def main():
                                " %(default)s)", type=int, default=1)
     args_optional.add_argument("-t", "--temp_dir", help="directory to use for temporary files" \
                                " (default: %(default)s)", default="/tmp")
-    
     args = parser.parse_args()
+    return args
+
+def main(args):
+    do_something()
+    # Read in genome; concatenate into individual long genome seq delimited by blocks of X's
+    # Read in miRNAs; break into individual files of appropriate length
+    # 
+
+    # Future work:
+    # Implement checkpointing; if some files are already done.
 
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    main(args)
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 textwidth=100 wrap
